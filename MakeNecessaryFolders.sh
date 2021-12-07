d6f83faa406f8de2c2816670842e238f010d85c4
Christopher Allen Greenberg Bonilla <c.greenberg@cern.ch>
2021-12-07 11:14:20 +0100

    Shell script to create all necessary folders.

---
+++ MakeNecessaryFolders.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+mkdir builds
+mkdir data
+cd data
+mkdir madgraph
+mkdir heppy
+cd madgraph
+mkdir output
+cd ../heppy
+mkdir output
+cd ../../
+
+mkdir results
+cd results
+mkdir heppy 
+cd heppy
+mkdir ratio_madgraph
+mkdir top_reco
+cd top_reco
+mkdir ATCGRoot
+
+cd ../../..
