681558f4c639351fab608704afdd7b7e351fca0d
Christopher GREENBERG <greenberg@lyoui1.in2p3.fr>
2021-12-17 20:31:45 +0100

    mod on runS

--- scripts/runSingleTopLHEAnalyzer.cpp
+++ scripts/runSingleTopLHEAnalyzer.cpp
@@ -14,19 +14,19 @@ using namespace std;
 
 int main()
 {
-  int nSamples = 21;
+  int nSamples = 4;
   int modes = 1;
   string* suffix = new string[nSamples];
   string* prefix = new string[modes];
 
-prefix[0]="RootFile";
+prefix[0]="t_channel_";
 //prefix[1]="madspin_";
 //prefix[0]="EVENTS_t_channel_madspin_Rwt_ctwi_cbwi_";
 
-// suffix[0] = "cbwi_n2";
-// suffix[1] = "cbwi_n1";
-// suffix[2] = "cbwi_p1";
-// suffix[3] = "cbwi_p2";
+suffix[0] = "cbw_m2";
+suffix[1] = "cbw_m1";
+suffix[2] = "cbw_p1";
+suffix[3] = "cbw_p2";
 // suffix[4] = "ctwi_p1";
 // suffix[5] = "ctwi_p2";
 // suffix[6] = "ctwi_n1";
@@ -39,7 +39,7 @@ prefix[0]="RootFile";
 //suffix[13] = "ctwi_m1";
 //suffix[14] = "ctwi_m2";
 //suffix[15] = "ctwi_p1";
-//suffix[16] = "ctwi_p2";
+
 
 
   TFile** fInput = new TFile*[nSamples];
@@ -50,12 +50,12 @@ prefix[0]="RootFile";
   {
     for (int i=0; i<nSamples; i++)
     {
-      string nb = to_string(i+1);
-      suffix[i]= nb;
+      //string nb = to_string(i+1);
+      //suffix[i]= nb;
       cout<<"Cooking "+prefix[j] + suffix[i]+".root"<<endl;
 
      ///////////////////////////Choose Input Path files//////////////////////////////////////
-     string inputPath = "/eos/user/c/cgreenbe/ROOTfiles/t_channel_EFT_atLO_nomadspin_ctw_cbw_cptb_ctwi_cbwi_cptbi_reweight_output/";
+     string inputPath = "/gridgroup/cms/greenberg/Pheno/MG5_aMC_v2_6_7/top_EFT/LHEconvertedFiles/";
      string inputName = inputPath + prefix[j] + suffix[i] + ".root";
       
       
@@ -66,7 +66,7 @@ prefix[0]="RootFile";
      singleTopLHEAnalyzer[i]->Loop();
       
      ///////////////////////////Choose Output Path files//////////////////////////////////////
-     string outputPath = "/eos/user/c/cgreenbe/ROOTfiles/CookedROOTfiles/t_channel_EFT_atLO_nomadspin_ctw_cbw_cptb_ctwi_cbwi_cptbi_reweight/";
+     string outputPath = "/gridgroup/cms/greenberg/Pheno/eft_lhe_analyzer/data/madgraph/CutData/Standalone/";
      string commandline = "mv output.root " + outputPath + outputName;
 
      system(commandline.c_str());
