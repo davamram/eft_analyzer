9e43b4d7ca96eb74436cb4460ccbae3035f28b8e
Christopher Greenberg <c.greenberg@cern.ch>
2022-01-13 15:43:34 +0100

    Created script to generate reweight cards

--- cardsGenproduction/reweightCardMaker.py
+++ cardsGenproduction/reweightCardMaker.py
@@ -1,35 +1,175 @@
+#########################################################################
+##              Script to write reweighting card for                   ##
+##                      genproductions MG5                             ##
+#########################################################################
+
+
+
 import argparse
 
 parser = argparse.ArgumentParser(description='Script to generate reweighting datacard for genproduction with MG5')
 args = parser.parse_args()
 
-reweightCard = open("_reweight_crad.dat","w+")
+def BranchWritter(name,cptb,cptbi,ctw,ctwi,cbw,cbwi):
+    reweightCard.write("launch --rwgt_name="+ name +"\n")
+    reweightCard.write("set DIM6 8 " + str(cptb) + " #cptb" "\n")
+    reweightCard.write("set DIM6 9 " + str(cptbi) + " #cptbi" "\n")
+    reweightCard.write("set DIM6 10 " + str(ctw) + " #ctw" "\n")
+    reweightCard.write("set DIM6 12 " + str(ctwi) + " #ctwi" "\n")
+    reweightCard.write("set DIM6 14 " + str(cbw) + " #cbw" "\n")
+    reweightCard.write("set DIM6 15 " + str(cbwi) + " #cbwi" "\n")
+    reweightCard.write("set decay 6 auto \n \n")
+
+def BranchNamer(Wname,Wvalue):
+    if Wvalue == 0: return ""
+    minusplus = "_p"
+    if Wvalue<0: minusplus = "_m"
+    return "_" + Wname + minusplus + str(Wvalue).removeprefix("-")
+
+
+reweightCard = open("reweight_crad.dat","w+")
 
 reweightCard.write("#****************************************************************** \n")
 reweightCard.write("#                       Reweight Module                           * \n")
 reweightCard.write("#****************************************************************** \n \n")
 reweightCard.write("change rwgt_dir rwgt \n \n")
 
-switch = "yes"
+###################### Only in Grid Points ######################
 
-WilsonCoeff = ["cptb","cptbi","ctw","ctwi","cbw","cbwi"]
-ParamCardCorresp = ["8","9","10","12","14","15"]
+# for i in range(-10,11,5):
+#     if i==0: continue
+#     minusplus = "p"
+#     value = str(i)
+#     if i<0: minusplus = "m"
+#     if len(str(i)) > 1: value = str(i).removeprefix("-")
+#     name = "cptb_" + minusplus + value
+#     BranchWritter(name,str(i),"0","0","0","0","0")
+
+# for i in range(-10,11,5):
+#     if i==0: continue
+#     minusplus = "p"
+#     value = str(i)
+#     if i<0: minusplus = "m"
+#     if len(str(i)) > 1: value = str(i).removeprefix("-")
+#     name = "cptbi_" + minusplus + value
+#     BranchWritter(name,"0",str(i),"0","0","0","0")
+
+# for i in range(-5,6,1):
+#     if (i==0 or i==-4 or i==-3 or i==3 or i==4): continue
+#     minusplus = "p"
+#     value = str(i)
+#     if i<0: minusplus = "m"
+#     if len(str(i)) > 1: value = str(i).removeprefix("-")
+#     name = "ctwi_" + minusplus + value
+#     BranchWritter(name,"0","0","0",str(i),"0","0")
+
+# for i in range(-2,3,1):
+#     if i==0: continue
+#     minusplus = "p"
+#     value = str(i)
+#     if i<0: minusplus = "m"
+#     if len(str(i)) > 1: value = str(i).removeprefix("-")
+#     name = "ctw_" + minusplus + value
+#     BranchWritter(name,"0","0",str(i),"0","0","0")
+
+# for i in range(-5,6,1):
+#     if (i==0 or i==-4 or i==-3 or i==3 or i==4): continue
+#     minusplus = "p"
+#     value = str(i)
+#     if i<0: minusplus = "m"
+#     if len(str(i)) > 1: value = str(i).removeprefix("-")
+#     name = "cbwi_" + minusplus + value
+#     BranchWritter(name,"0","0","0","0","0",str(i))
+
+# for i in range(-2,3,1):
+#     if i==0: continue
+#     minusplus = "p"
+#     value = str(i)
+#     if i<0: minusplus = "m"
+#     if len(str(i)) > 1: value = str(i).removeprefix("-")
+#     name = "cbw_" + minusplus + value
+#     BranchWritter(name,"0","0","0","0",str(i),"0")
+
+###################### Whole Grid Points ######################
+
+separator = "\n##############################################################\n############################################################## \n \n"
+step = 640
+count = 0
+cuts1 = [-5,-4,-3,0,3,4,5]
+cuts2 = [-10,-5,5,10]
+listOfProcesses = []
+
+for cptb in range(-10,11,5):
+    if cptb in cuts2:
+        name_cptb = ""
+        cptb = 0
+        continue
+    name_cptb = BranchNamer("cptb",cptb)
+
+    for cptbi in range(-10,11,5):
+        if cptbi in cuts2:
+            name_cptbi = ""
+            cptbi = 0
+        name_cptbi = BranchNamer("cptbi",cptbi)
+
+        for ctw in range(-5,6,1):
+            if ctw in cuts1:
+                name_ctw = ""
+                ctw = 0
+                continue
+            name_ctw = BranchNamer("ctw",ctw)
+
+            for ctwi in range(-5,6,1):
+                if ctwi in cuts1:
+                    name_ctwi = ""
+                    ctwi = 0
+                    continue
+                name_ctwi = BranchNamer("ctwi",ctwi)
+
+                for cbw in range(-5,6,1):
+                    if cbw in cuts1:
+                        name_cbw = ""
+                        cbw = 0
+                        continue
+                    name_cbw = BranchNamer("cbw",cbw)
+
+                    for cbwi in range(-5,6,1):
+                        if cbwi in cuts1:
+                            name_cbwi = ""
+                            cbwi = 0
+                            continue
+                        name_cbwi = BranchNamer("cbwi",cbwi)
+
+                        process = (name_cptb + name_cptbi + name_ctw + name_ctwi + name_cbw + name_cbwi).removeprefix("_")
+                        if process == "": process = "SM"
+                        if count%step == 0: #when count = n*step -> count%step = 0
+                            reweightCard.write(separator)
+                            reweightCard.write("#Number of precesses written: " + str(count) + "\n \n")
+                        BranchWritter(process,cptb,cptbi,ctw,ctwi,cbw,cbwi)
+
+                        count += 1
+
+print("Number of Processes = ", count)
 
 
+######################Make your own process manually######################
 
-while switch == "yes":
-    switch = ""
-    branchName = input("Enter branch name:")
-    reweightCard.write("launch --rwgt_name="+branchName+"\n")
+# switch = input("Do you want to add a new branch? [yes/no]: ")
+# WilsonCoeff = ["cptb","cptbi","ctw","ctwi","cbw","cbwi"]
+# ParamCardCorresp = ["8","9","10","12","14","15"]
+# while switch == "yes":
+#     switch = ""
+#     branchName = input("Enter branch name:")
+#     reweightCard.write("launch --rwgt_name="+branchName+"\n")
 
-    for i in range(len(WilsonCoeff)):
-        CoeffValue = input(WilsonCoeff[i] + " = ")
-        reweightCard.write("set DIM6 " + ParamCardCorresp[i] + " " + CoeffValue + "    #" + WilsonCoeff[i] + "\n")
+#     for i in range(len(WilsonCoeff)):
+#         CoeffValue = input(WilsonCoeff[i] + " = ")
+#         reweightCard.write("set DIM6 " + ParamCardCorresp[i] + " " + CoeffValue + "    #" + WilsonCoeff[i] + "\n")
 
-    reweightCard.write("set decay 6 auto \n \n")
-    switch = input("Do you want to add a new branch? [yes/no]")
+#     reweightCard.write("set decay 6 auto \n \n")
+#     switch = input("Do you want to add a new branch? [yes/no]")
 
-reweightCard.close()
+# reweightCard.close()
 
             
 
