d646f5698bfbf73528f785bbaf45a0b6a1751263
Christopher GREENBERG <greenberg@lyoserv2.in2p3.fr>
2022-05-27 11:41:44 +0200

    Modify Input and Output pathways

--- scripts/runSingleTopLHEAnalyzer.cpp
+++ scripts/runSingleTopLHEAnalyzer.cpp
@@ -14,14 +14,20 @@ using namespace std;
 
 int main()
 {
-  int nSamples = 1;
+  int nSamples = 6;
   int modes = 1;
   string* suffix = new string[nSamples];
   string* prefix = new string[modes];
 
-prefix[0]="t_channel_"; // Name of input File
+  prefix[0]="ST_t_channel_atLO_500Kevents_"; // Name of input File
 
-suffix[0] = "EFT_atLO_nomadspin_reweight729";
+  suffix[0] = "SM_MG5_dim6top";
+  suffix[1] = "allDIM6_p2p5_MG5_dim6top";
+  suffix[2] = "cbwi_m2_MG5_dim6top";
+  suffix[3] = "cptb_m5_cptbi_m5_ctw_m2_ctwi_m2_cbw_m2_cbwi_m2_MG5_dim6top";
+  suffix[4] = "cptbi_p5_ctw_p2_cbw_p2_MG5_dim6top";
+  suffix[5] = "ctwi_m2_MG5_dim6top";
+
 
   TFile** fInput = new TFile*[nSamples];
   TTree** tInput = new TTree*[nSamples];
@@ -36,18 +42,18 @@ suffix[0] = "EFT_atLO_nomadspin_reweight729";
       cout<<"Cooking "+prefix[j] + suffix[i]+".root"<<endl;
 
      ///////////////////////////Choose Input Path files//////////////////////////////////////
-     string inputPath = "/eos/lyoeos.in2p3.fr/home/greenberg/rootFiles/nonCooked/t_channel_EFT_atLO_nomadspin_reweight_729_5M/";
+     string inputPath = "/eos/lyoeos.in2p3.fr/home/greenberg/rootFiles/";
      string inputName = inputPath + prefix[j] + suffix[i] + ".root";
       
       
-     string outputName = "output_" +prefix[j] + suffix[i] +  ".root";   
+     string outputName = "output_" +prefix[j] + suffix[i] +  ".root";
      fInput[i] = new TFile(inputName.c_str(),"READ");
      tInput[i] = (TTree*) fInput[i]->Get("LHEF");
      singleTopLHEAnalyzer[i] = new SingleTopLHEAnalyzer(tInput[i]);
      singleTopLHEAnalyzer[i]->Loop();
       
      ///////////////////////////Choose Output Path files//////////////////////////////////////
-     string outputPath = "/eos/lyoeos.in2p3.fr/home/greenberg/rootFiles/cookedFiles/";
+     string outputPath = "/eos/lyoeos.in2p3.fr/home/greenberg/rootFiles/cookedFiles/StReco17/lhapdf306000/";
      string commandline = "mv output.root " + outputPath + outputName;
 
      system(commandline.c_str());
